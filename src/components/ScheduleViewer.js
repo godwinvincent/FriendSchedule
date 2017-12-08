1edd970daa46d4dbc31c79a208a236a1a7fbcc8a
avania <avani.amin96@gmail.com>
2017-12-07 18:50:56 -0800

    Need to commit to pull

--- src/components/ScheduleViewer.js
+++ src/components/ScheduleViewer.js
@@ -1,15 +1,66 @@
-import React, { Component } from 'react';
-import { NavBar } from './Navigation';
+// import React, { Component } from 'react';
+// import { NavBar } from './Navigation';
+// import { Table } from 'reactstrap'
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/database';
+// import { StyleSheet, css } from 'aphrodite/no-important';
+// import * as colors from '../styles/colors';
 
-import { StyleSheet, css } from 'aphrodite/no-important';
-import * as colors from '../styles/colors';
+// export class ScheduleViewer extends Component {
+//     render() {
+//         return (
+//             <Table>
+//                 <thead>
+//                     <tr>
+//                     <th>Class Name</th>
+//                     <th>Friend List</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                 </tbody>
+//             </Table>
+//         )
+//     }
+// }
 
-export class ScheduleViewer extends Component {
-    
+// class ClassList extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
 
+//     componentDidMount() {
+//         this.userRef = firebase.database().ref('Users/' + this.props.fbID);
+//         this.userRef.on('value', (snapshot) => {
+//             let val = snapshot.val();
+//             let courseObj = {};
+//             courseObj['tester'] = val;
+//             this.setState(courseObj);
+//         });
+//     }
 
+//     componentWillUnmount() {
+//         this.userRef.off();
+//     }
 
+//     render() {
+//         let userId = 'tester';
+//         if (this.state[userId]) {
+//             let courseIds = Object.keys(this.state[userId]);
+//             let courseItems = courseIds.map((courseId) => {
+//                 let course = this.state[userId][courseId];
+//                 course.id = courseId;
+//                 return <ClassItem userId={this.props.fbID} key={course.id} course={course} currentUser={this.props.fbID} />
+//             });
+//         }
+//     }
+// }
 
-
-
-}
+// class ClassItem extends Component {
+//     render() {
+//         return(
+//             <td key={this.props.key}> </td>
+//         )
+//     }
+// }

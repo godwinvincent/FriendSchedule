08fc8bd40b1bd93be834866b128a8d97c538521a
Godwin Vincent <godwinvincent@gmail.com>
2017-12-10 13:30:50 -0800

    added basename prop

--- src/components/Login.js
+++ src/components/Login.js
@@ -67,6 +67,7 @@ export default class Login extends Component {
             var email = error.email;
             // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
+            console.log(error)
             // ...
           });
     }
